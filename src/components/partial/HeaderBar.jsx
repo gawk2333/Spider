40f74dd4f428a8b0f568e06a8d8c0e97b4127ba3
Kang Gao <kang.g1988@gmail.com>
2022-12-29 14:01:50 +1300

    Step2 use react-select instead of original html select element

--- src/components/partial/HeaderBar.jsx
+++ src/components/partial/HeaderBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import Select from 'react-select'
 
 export default function HeaderBar({ mode, setMode}) {
+  const options = [
+    {value:0, label:'Easy'},
+    {value:1, label:'Midium'},
+    {value:2, label:'Hard'}
+  ]
   return (
     <div className='App-header'>
       <div className='App-logo'>Spider Solitaire</div>
-      <select className='modeSelector' defaultValue={1}>
-        <option value={0}>Easy</option>
-        <option value={1}>Middle</option>
-        <option value={2}>Hard</option>
-      </select>
+      <div className='spacer'/>
+      <Select 
+      className='modeSelector'
+      defaultValue={1}
+      options={options}/>
     </div>
   )
 }
